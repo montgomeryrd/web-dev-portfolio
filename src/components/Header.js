8742a2f882900876ba03c121810d2f2b6ed1f55c
montgomeryrd <montgomery_rd@yahoo.com>
2021-03-29 20:18:15 -0700

    homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState } from 'react';
+import me from '../assets/images/../../assets/images/me.jpg';
+import './Header.css';
 
 const Header = () => {
+    const [modal, setModal] = useState(false);
+
     return (
-        <div>
-            
-        </div>
+        <header>
+            <div className="hamburger-menu" onClick={() => {setModal(prev => !prev)}}>
+                <div className="hamburger-lines"></div>
+            </div>
+
+            <div className="hamburger-modal" style={{display: modal ? 'flex' : 'none'}}>
+                <h2>Projects</h2>
+                <h2>About</h2>
+                <h2>Education</h2>
+            </div>
+
+            <div className="img-container">
+                <img className="profile-pic" src={me} alt="profile pic" />
+            </div>
+        </header>
     )
 }
 
-export default Header
+export default Header;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,23 @@
+import pexels from '../assets/videos/pexels-pavel-danilyuk-5495781.mp4';
 import './Home.css';
 
 const Home = () => {
     return (
         <div id="home-container" className="desktop mobile" style={{height: `${100}vh`}}>
             <main className="home-content" role="main">
-                <h2 className="home-intro">Hi, my name is</h2>
-                <h1 className="home-name">RICHARD MONTGOMERY</h1>
-                <p className="home-text">
-                    <span id="home-text-one">I am a web developer </span>
-                    <span id="home-text-two">living on the Kitsap Peninsula </span>
-                    <span id="home-text-three">in Washington State. </span>
-                    <span id="home-text-four">I enjoy building </span>
-                    <span id="home-text-five">functional web applications </span>
-                    <span id="home-text-six">using interactive, </span>
-                    <span id="home-text-seven">adaptive and responsive design.</span>
-                </p>
+                <div className="video-background">
+                    <div className="overlay"></div>
+                    <video src={pexels} width={`${220}%`} muted loop autoPlay></video>
+                </div>
+                <div className="home-text">
+                    <h2 className="home-intro">Hi, my name is</h2>
+                    <h1 className="home-name">RICHARD MONTGOMERY</h1>
+                    <p className="home-paragraph">
+                        I am a web developer living on the Kitsap Peninsula 
+                        in Washington State. I enjoy building functional web 
+                        applications using interactive, adaptive and responsive design.
+                    </p>
+                </div>
             </main>
         </div>
     );
