4e7bfff8c68cd3310dc1d044280dafefc0ac5600
montgomeryrd <montgomery_rd@yahoo.com>
2021-03-31 13:40:15 -0700

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import './Footer.css';
 
 const Footer = () => {
+    const copyEmailAddress = () => {
+        const emailAddress = document.querySelector('.email-text');
+        const textArea = document.createElement('textarea');
+        textArea.value = emailAddress.textContent;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('Copy');
+        textArea.remove();
+    };
+
+    const copiedAlert = () => {
+        const alert = document.querySelector('.copied-box');
+        setTimeout(() => {
+            alert.style = 'display: block';
+        }, 1000);
+    }
+    
     return (
-        <div>
+        <footer className="footer-container">
+            <section className="email">
+                <div className="copied-box">
+                    <div className="copied-speech-bubble">
+                        <span className="copied-alert">copied</span>
+                    </div>
+                    <div className="copied-speech-bubble-triangle"></div>
+                </div>
+                <span className="email-text" onClick={() => copyEmailAddress()}>montgomery_rd@yahoo.com</span>
+            </section>
+            <section>
+                <a className="ml-3 p-3" href="https://github.com/montgomeryrd" target="_blank" rel="noreferrer">
+                    <i className="fab fa-github-alt"></i>
+                </a>
             
-        </div>
+                <a className="p-3" href="https://www.linkedin.com/in/montgomeryrd/" target="_blank" rel="noreferrer">
+                    <i className="fab fa-linkedin-in"></i>
+                </a>
+        
+                <a className="p-3" href="https://twitter.com/richard04755318" target="_blank" rel="noreferrer">
+                    <i className="fab fa-twitter"></i>
+                </a>
+            </section>
+        </footer>
     )
 }
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,47 +1,56 @@
 import pexels from '../assets/videos/pexels-pavel-danilyuk-5495781.mp4';
+// import html5 from '../assets/icons/html5.png';
+// import css3 from '../assets/icons/css3.png';
+// import javascript from '../assets/icons/javascript.png';
+// import react from '../assets/icons/react.png';
+// import node from '../assets/icons/nodejs.png';
+// import mongodb from '../assets/icons/mongodb.png';
+// import sass from '../assets/icons/sass.png';
 import './Home.css';
 
 const Home = () => {
     return (
         <div id="home-container" className="desktop mobile" style={{height: `${100}vh`}}>
             <main className="home-container" role="main">
+
+                <video src={pexels} muted loop autoPlay></video>
+                <div className="overlay"></div>
+
                 <section className="home-jumbotron">
-                    <video src={pexels} muted loop autoPlay></video>
-                    <div className="overlay"></div>
-                    <div className="home-jumbotron-text">
-                        <h2 className="home-intro">Hi, my name is</h2>
-                        <h1 className="home-name">RICHARD MONTGOMERY</h1>
-                        <p className="home-paragraph">
-                            I am a web developer living on the Kitsap Peninsula 
-                            in Washington State. I enjoy building functional web 
-                            applications using interactive, adaptive and responsive design.
-                        </p>
-                    </div>
+                    <h2 className="home-intro">Hi, my name is</h2>
+                    <h1 className="home-name">RICHARD MONTGOMERY</h1>
+                    <p className="home-paragraph">
+                        I am a web developer living on the Kitsap Peninsula 
+                        in Washington State. I enjoy building functional web 
+                        applications using interactive, adaptive and responsive design.
+                    </p>
                 </section>
-                
+
                 <section className="alt-links">
-                    <div className="github links">
-                        <a href="https://github.com/montgomeryrd" target="_blank" rel="noreferrer">
-                            <i className="icons fab fa-github-square"></i>
-                        </a>
-                    </div>
-                    <div className="linkedin links">
-                        <a href="https://www.linkedin.com/in/montgomeryrd/" target="_blank" rel="noreferrer">
-                            <i class="icons fab fa-linkedin-in"></i>
-                        </a>
-                    </div>
-                    <div className="twitter links">
-                        <a href="https://twitter.com/ReallyGreenCode" target="_blank" rel="noreferrer">
-                            <i className="icons fab fa-twitter"></i>               
-                        </a>
-                    </div>
-                    <div className="resume links">
-                        <a href="https://docs.google.com/document/d/14xeZcrS5G_fks0mnwhQnKD8FiJymT83mCovkMyAKpTY/edit?usp=sharing" target="_blank" rel="noreferrer">
-                            <i className="icons fas fa-code"></i>   
-                        </a>
-                    </div>
-                    <div className="blank link"></div>
+                    <a className="github links" href="https://github.com/montgomeryrd" target="_blank" rel="noreferrer">
+                        <i className="icons fab fa-github-square"></i>
+                    </a>
+                    <a className="linkedin links" href="https://www.linkedin.com/in/montgomeryrd/" target="_blank" rel="noreferrer">
+                        <i className="icons fab fa-linkedin-in"></i>
+                    </a>
+                    <a className="twitter links" href="https://twitter.com/ReallyGreenCode" target="_blank" rel="noreferrer">
+                        <i className="icons fab fa-twitter"></i>               
+                    </a>
+                    
+                    <a className="resume links" href="https://docs.google.com/document/d/14xeZcrS5G_fks0mnwhQnKD8FiJymT83mCovkMyAKpTY/edit?usp=sharing" target="_blank" rel="noreferrer">
+                        <i className="icons fas fa-code"></i>   
+                    </a>
                 </section>
+
+                {/* <section className="home-libraries">
+                    <img src={html5} alt="HTML5" />
+                    <img src={css3} alt="CSS3" />
+                    <img src={javascript} alt="JavaScript" />
+                    <img src={react} alt="React" />
+                    <img src={node} alt="Node.js" />
+                    <img src={mongodb} alt="MongoDB" />
+                    <img src={sass} alt="SASS" />
+                </section> */}
             </main>
         </div>
     );
