e2cf8a5f96c2ce6efdb5b07e8976d0e6a9ddce9c
montgomeryrd <montgomery_rd@yahoo.com>
2021-04-08 20:47:15 -0700

    trying to work out codewars API

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,8 +1,12 @@
+import './About.css';
+
 const About = () => {
     return (
         <div id="about-container" className="desktop mobile" style={{height: `${100}vh`}}>
             <main className="about-content" role="main">
-                
+                <h2>technical</h2>
+
+                <h2>personal</h2>
             </main>
         </div>
     )
