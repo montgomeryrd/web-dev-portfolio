c38a439f492c9223a2114403aa43cfb5cb2f0587
montgomeryrd <montgomery_rd@yahoo.com>
2021-04-12 21:50:17 -0700

    working on some wiring

---
+++ src/components/SectionOne.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sections.css';
+
+const SectionOne = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SectionOne;
---
+++ src/components/SectionThree.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Sections.css';
+
+const SectionThree = () => {
+    return (
+        <section className="desktop mobile">
+            <ul className="section-three-fontawesome-icons" style={{listStyle: "none"}}>
+                <li><i className="fab fa-github"></i></li>
+                <li><i className="fab fa-html5"></i></li>
+                <li><i className="fab fa-css3"></i></li>
+                <li><i className="fab fa-js"></i></li>
+                <li><i className="fab fa-node"></i></li>
+                <li><i className="fab fa-react"></i></li>
+                <li><i className="fas fa-database"></i></li>
+                <li><i className="fab fa-mdb"></i></li>
+                <li><i className="fab fa-sass"></i></li>
+            </ul>
+        </section>
+    )
+}
+
+export default SectionThree;
